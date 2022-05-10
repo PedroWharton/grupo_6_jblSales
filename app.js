aa2d755a64cf8cbaf3f0228191d97f2911210a22
PedroWharton <pedrowharton@gmail.com>
2022-05-09 23:08:08 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const publicPath = path.resolve(__dirname, "./public")
+
+app.listen(3000, function(){
+    console.log("Servidor corriendo");
+})
+
+app.get("/", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+})
