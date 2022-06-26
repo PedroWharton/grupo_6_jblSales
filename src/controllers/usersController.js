4ba75f95c8796b65a3370b4de5ff08c4603022e9
PedroWharton <pedrowharton@gmail.com>
2022-06-26 15:00:32 -0300

    funcionalidad de registro y validaciones

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,18 +2,43 @@ const express = require('express');
 const router = require('../routes/mainRouter');
 const path = require('path');
 const fs = require('fs');
+const { validationResult } = require('express-validator');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const usersController ={
     login: function(req, res){
         res.render('./user/login');
     },
     register: function(req, res){
-        res.render('./user/register.ejs');
-    }
+        res.render('./user/register');
+    },
 
+    registerFunction: function(req, res){
+        const resultValidation = validationResult(req);
+        if(resultValidation.errors.length > 0){
+            res.render('./user/register', {
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            })
+        }
+        else{
+            let newUser= req.body;
+		newUser.id = users[users.length -1].id + 1;
+		newUser.avatar = "Logo.png";
+        newUser.admin = false;
+		users.push(newUser)
+        console.log(users)
+		let jsonUsers = JSON.stringify(users)
+		fs.writeFileSync(usersFilePath, jsonUsers)
+		res.render('index', { products })
+        }     
+    }
 }
 
 module.exports = usersController;
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
+const { body } = require('express-validator');
+
+const validations = [
+    body('username').notEmpty().withMessage('Tenes que escribir un nombre de usuario'),
+
+    body('email').notEmpty().withMessage('Tenes que escribir un email').bail()
+    .isEmail().withMessage('Debes escribir un email valido'),
+
+    body('password').notEmpty().withMessage('Tenes que escribir una contraseña').bail()
+    .isLength({ min: 7 ,max: 20 }).withMessage('La contraseña debe tener entre 7 y 20 caracteres').bail()
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, "i")
+    .withMessage('La contraseña debe contener una mayuscula y un numero'),
+
+    body('pswRepeat').custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Las contraseñas deben coincidir');
+        }
+    
+        // Indicates the success of this synchronous custom validator
+        return true;
+      })
+];
 
 
 router.get('/login', usersController.login);
+
 router.get('/register', usersController.register);
+router.post('/register', validations,  usersController.registerFunction);
 
 module.exports = router;
