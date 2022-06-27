7c44aa8a59e188ee2c07cf0622c0eb9ad91f0a50
PedroWharton <pedrowharton@gmail.com>
2022-06-27 13:59:29 -0300

    agrego search

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -9,6 +9,17 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const mainController ={
     index: function(req, res){
         res.render('index', { products });
+    },
+    search: function(req, res){
+        let qs = req.query.query;
+		console.log(qs)
+		let busqueda = [];
+		for(let product of products){
+			if(product.name.includes(qs) || product.description.includes(qs)){
+				busqueda.push(product);
+			}
+		}
+        res.render('./products/products', { products: busqueda, qs })
     }
 
 }
