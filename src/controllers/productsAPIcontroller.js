4e0cb228bba3d83fbe02c39bc02adc16d70429e6
PedroWharton <pedrowharton@gmail.com>
2022-09-26 13:19:04 -0300

    termino con all products, avanzo con product detail API

--- src/controllers/productsAPIcontroller.js
+++ src/controllers/productsAPIcontroller.js
@@ -2,13 +2,21 @@ const db = require('../../database/models');
 
 const productsAPIcontroller ={
     getProducts: function(req,res){
-        db.Product.findAndCountAll().then(function(result){
+        db.Product.findAndCountAll({
+            include: [
+                {
+                    association: "category"
+                }
+
+              ]
+            }).then(function(result){
+                console.log(result);
             let products = []
             result.rows.map(e => products.push({
                 id: e.product_id,
                 name: e.name,
                 description: e.description,
-                relaciones: [],
+                categories: [e.category.name],
                 detail: `loalhost:300/api/products/${e.product_id}`
             }))
             const response = {
@@ -21,7 +29,17 @@ const productsAPIcontroller ={
         })  
     },
     productDetail: function(req,res){
-        db.Product.findOne({where: {product_id: req.params.id}}).then(function(result){
+        db.Product.findOne({where: {product_id: req.params.id}},{
+            include: [
+                {
+                    association: "category",
+                    where: {'$Category.category_id$': 'Product.category_id'}
+                }
+
+              ]
+            }).then(function(result){
+
+                console.log(result)
             let product = {
                 id: result.product_id,
                 name: result.name,
